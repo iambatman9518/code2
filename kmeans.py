ab99493d7475c4e53da995010d194e49725ca4a7
iambatman9518 <iambatman9518@gmail.com>
2024-11-06 01:32:45 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,36 +1,21 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
-x = [4, 5, 10, 4, 3, 11, 14, 6, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-data = list(zip(x, y))
+df = pd.read_csv('creditcard.csv')
+df = df.dropna()
 
-plt.scatter(x, y)
-plt.title('Scatter Plot of Data Points')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.show()
+X = df[['Feature1', 'Feature2']]
 
-inertias = []
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i, random_state=42)
-    kmeans.fit(data)
-    inertias.append(kmeans.inertia_)
+kmeans = KMeans(n_clusters=3, random_state=42)
+kmeans.fit(X)
 
-plt.plot(range(1, 11), inertias, marker='o')
-plt.title('Elbow Method for Optimal k')
-plt.xlabel('Number of clusters')
-plt.ylabel('Inertia')
-plt.show()
+df['Cluster'] = kmeans.labels_
 
-optimal_k = 2  
-kmeans = KMeans(n_clusters=optimal_k, random_state=42)
-kmeans.fit(data)
-
-plt.scatter(x, y, c=kmeans.labels_, cmap='viridis', marker='o')
-plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', marker='x', s=200, label='Centroids')
-plt.title('K-means Clustering Results')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
+plt.scatter(X['Feature1'], X['Feature2'], c=df['Cluster'], cmap='viridis')
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red', label='Centroids')
+plt.xlabel('Feature1')
+plt.ylabel('Feature2')
 plt.legend()
+plt.title("K-Means Clustering")
 plt.show()
